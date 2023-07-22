8853acb3c02ee6f1e57a5087cadc8a32b647a38e
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-07-22 18:02:22 +1200

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,36 @@
 echo "
+                         .=#%@@@@%*-                         
+                       *@@@@@@@@@#%@*.                      
+                      #@@@@@@@@@@#%@@@:                     
+                     :@@@@@@@@@@@@@@@@@                     
+                     =@%##@@@@%*++%@@@@=                    
+                     =%:+:-@@+ ++-:@@@@+                    
+                     =@-@@:#*+:@@% @@@@+                    
+                     -@+===----==:=@@@@*                    
+                     :@+===-----=++@@@@@                    
+                      @#++====++===@@**@*                   
+                     -@+:-+++==-.  :@@%@@*                  
+                   .*@+  .:::..     .@@@@@%:                
+                  =@@-               -@@@@@@+               
+                .%@@+               . %@@@@@@@-             
+               .@@@@-               ...+@@@@@@@+            
+               #@@%.                    =@%%%@@@=           
+              =@@@.                      #%@@%@@@.          
+             -@%@-                       -@@@#@@@*          
+            -@@@%                        :@@@%@@@@          
+           :@@%%+                        :@@%@@@@@:         
+           :%##%+                        =@@@@@%%@          
+           -====#%=                    ===@@@@@@%=          
+      ----=======*@@+.                .+==%@@@@#===         
+     :============+@@@#:             .:+===+++==--=:        
+     .+=============%@@%             :*+=-====-----=-:      
+     .+==============+:            :*@%+=-----------==+.    
+     =================+=:.     .-+%@@@#+=------===+++-.     
+    .+++==============+#@@@@@@@@@@@@@@#+======++*=:         
+       .:-==++++=====+**=-:::::--=+*#%#**++++*+:            
+"
+sleep 6
+echo "
 ████████╗██╗░░░██╗██████╗░██████╗░░█████╗░████████╗██╗░░░██╗██╗░░██╗
 ╚══██╔══╝██║░░░██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║░░░██║╚██╗██╔╝
 ░░░██║░░░██║░░░██║██████╔╝██████╦╝██║░░██║░░░██║░░░██║░░░██║░╚███╔╝░
@@ -31,12 +63,18 @@ sudo apt remove --purge --assume-yes snapd gnome-software-plugin-snap
 rm -rf ~/snap/
 sudo rm -rf /var/cache/snapd/ 
 
+echo "remove ubuntu wallpapers"
+sudo apt remove --purge ubuntu-wallpapers*
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https -y
+
 echo "Add Repositories (to replace snaps)."
 echo "deb [trusted=yes] https://turbotuxlinux.github.io/repo/ stable main" > /etc/apt/sources.list.d/turbotuxlinux.list
 sudo add-apt-repository ppa:mozillateam/ppa -y
 sudo add-apt-repository ppa:thopiekar/openrgb -y
 sudo add-apt-repository ppa:stk/dev -y
-sudo add-apt-repository ppa:antimicrox/ppa -y
+sudo add-apt-repository ppa:libretro/stable -y
 sudo add-apt-repository multiverse -y
 echo '
 Package: *
@@ -48,4 +86,22 @@ sudo apt update
 
 echo "Install most things"
 sudo apt install linux-headers-6.1.38turbotux linux-image-6.1.38turbotux linux-libc-6.1.38turbotux
-sudo apt install firefox obs-studio kdenlive lutris openrgb steam antimicrox -y
+sudo apt install firefox obs-studio kdenlive lutris openrgb steam sc-controller minecraft-launcher retroarch flatpak -y
+
+echo "flatpak"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub hu.kramo.Cartridges 
+
+echo "Customize logos, because why not?"
+sudo cp /usr/share/plymouth/ubuntu-logo.png{,.bak}
+sudo cp ./watermark.png /usr/share/plymouth/ubuntu-logo.png
+sudo cp ./watermark.png /usr/share/plymouth/themes/spinner/watermark.png
+sudo cp ./logo.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
+
+echo "Pin stuff to dash"
+echo '
+[org.gnome.shell]
+favorite-apps = "['firefox.desktop', 'org.gnome.Nautilus.desktop']"
+' > /usr/share/glib-2.0/schemas/90_ubuntu-settings.gschema.override
+
+echo "I think it's done"
